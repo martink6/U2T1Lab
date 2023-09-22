96daf683c1016a5a7ae62c61c9f364dfa1efa403
BT_2N2_17 <sample@bths.edu>
2023-09-22 13:40:56 -0400

    Rectangle

---
+++ src/RectangleRunner.java
@@ -0,0 +1,18 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+
+        // creating one instance/object from the Rectangle class
+        Rectangle rect1 = new Rectangle(5, 6);
+        rect1.printArea();  // calling a method on the object
+
+        // creating ANOTHER instance/object from the Rectangle class
+        Rectangle rect2 = new Rectangle(10, 8);
+        rect2.printArea();  // calling a method on the object
+
+        Rectangle rect3 = new Rectangle(11, -2);
+        rect3.printArea();
+    }
+}
+
+
+
